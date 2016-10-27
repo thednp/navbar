f1515768450ae28bf6e9d6a74b6e11b309ac4628
thednp <thednp@users.noreply.github.com>
2016-10-27 12:54:43 +0300

    Some optimization, timer is no longer stored in menu item attributes.

--- navbar.js
+++ navbar.js
@@ -6,7 +6,8 @@
 var Navbar = function(el) {
 	var menu = (typeof el === 'object') ? el : document.querySelector(el), self = this,
 		items = menu.getElementsByTagName('LI'), il = items.length,
-    isIE = (new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null) ? parseFloat( RegExp.$1 ) : false;
+		isIE = (new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null) ? parseFloat( RegExp.$1 ) : false,
+		events = ('onmouseleave' in document) ? ['mouseenter', 'mouseleave'] : ['mouseover', 'mouseout'];
 
 	function addClass(l,c) { // where modern browsers fail, use classList	
 		if (l.classList) { l.classList.add(c); } else { l.className += ' '+c; l.offsetWidth; }
@@ -14,17 +15,17 @@ var Navbar = function(el) {
 	function removeClass(l,c) {
 		if (l.classList) { l.classList.remove(c); } else { l.className = l.className.replace(c,'').replace(/^\s+|\s+$/g,''); }
 	}
-	
+
 	// methods
 	this.init = function(l) {
-		l.addEventListener("mouseenter", this.enter, false);
-		l.addEventListener("mouseleave", this.leave, false);	
-	},
+		l.addEventListener(events[0], this.enter, false);
+		l.addEventListener(events[1], this.leave, false);	
+	};
 	this.enter = function() {
 		var that = this; // this is now the event target, the LI
-		clearTimeout(this.getAttribute('data-timer'));
+		clearTimeout(this.timer);
 		if ( !/\bopen/.test(this.className) ) {
-			self.timer = setTimeout( function() {
+			this.timer = setTimeout( function() {
 				addClass(that,'open'); 
 				addClass(that,'open-position');                        
 				var s = that.parentNode.childNodes; //all parentNode children
@@ -39,21 +40,19 @@ var Navbar = function(el) {
 					}	
 				}
 			}, 100 );	
-			this.setAttribute('data-timer',self.timer);
 		}
-	},
+	};
 	this.leave = function() {
 		var that = this;
-		clearTimeout(this.getAttribute('data-timer'));
-		self.timer = setTimeout( function() {
+		clearTimeout(this.timer);
+		this.timer = setTimeout( function() {
 			if (that && that.className && /open/.test(that.className) && that.querySelector('.form-control') !== document.activeElement ) {
 				removeClass(that,'open');
 				setTimeout(function(){ removeClass(that,'open-position'); }, (isIE ? 0 : 200))
 			}
 		}, 500);
-		this.setAttribute('data-timer',self.timer);	
-	},
-	this.timer = null;
+	};
+	// init
 	for ( var i=0; i<il; i++ ) {
 		if ( items[i].getElementsByTagName('UL').length !== 0 ) {
 			this.init(items[i]);
