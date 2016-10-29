19f03cd19a773c3895bcebc3479e8e9dd494a48b
thednp <thednp@users.noreply.github.com>
2016-10-29 22:45:07 +0300

    switching to sass.js for scss build script

--- build-scss.js
+++ build-scss.js
@@ -4,40 +4,40 @@
 // Run node build-scss.js --help for usage instructions
 
 var fs = require('fs');
-var sass = require('node-sass');
+var sass = require('sass.js');
 var pack = require('./package.json');
 var version = 'v'+pack.version;
 var license = pack.license+'-License';
 
 var variables = fs.readFileSync('SCSS/variables.scss').toString();
+var style = fs.readFileSync('SCSS/navbar.scss').toString();
 
 console.log('Compiling navbar.scss ' + version + '..');
 
 // Helper Functions:
-function sassify(custom, srcPath, writePath, compressType) {
-  sass.render({
-    data: ( custom + variables + fs.readFileSync(srcPath).toString() ),
-    outputStyle: compressType
-  }, function(error, result) {
-    if (error) {
-      console.log(error.status);
-      console.log(error.column);
-      console.log(error.message);
-      console.log(error.line);
-    }  else {
-      fs.writeFile(writePath, '/* navbar.js ' + version + ' | © dnp_theme | ' + license + '*/\n' + result.css);
+function sassify(custom, writePath, compressType) {
+  sass.compile(( custom + variables + style ), {
+    style: sass.style[compressType]
+  }, function callback(result) {
+    if (result.status === 0) {
+      fs.writeFile(writePath, '/* navbar.js ' + version + ' | © dnp_theme | ' + license + '*/\n' + result.text);
       console.log(writePath+' is done.');
+    } else {
+      console.log(result.status);
+      console.log(result.column);
+      console.log(result.message);
+      console.log(result.line);
     }
   });
 }
 
 // Compile CSS and prepare distributions
-sassify('','SCSS/navbar.scss', 'navbar.css', 'compact'); // non-minified
-sassify('','SCSS/navbar.scss', 'dist/navbar.min.css', 'compressed'); // minified
+sassify('', 'navbar.css', 'compact'); // non-minified
+sassify('', 'dist/navbar.min.css', 'compressed'); // minified
 
 
 // prepare demos
-sassify('','SCSS/navbar.scss', 'demo/src/css/navbar.min.css', 'compressed'); // main-menu
-sassify('$brand_color: #CD3232; $nav_layout: "left-side"; ','SCSS/navbar.scss', 'demo/src/css/navbar-left.min.css', 'compressed'); // left-side
-sassify('$brand_color: #8032CD; $nav_layout: "right-side"; ','SCSS/navbar.scss', 'demo/src/css/navbar-right.min.css', 'compressed'); // right-side
+sassify('', 'demo/src/css/navbar.min.css', 'compressed'); // main-menu
+sassify('$brand_color: #CD3232; $nav_layout: "left-side";', 'demo/src/css/navbar-left.min.css', 'compressed'); // left-side
+sassify('$brand_color: #8032CD; $nav_layout: "right-side";', 'demo/src/css/navbar-right.min.css', 'compressed'); // right-side
 
